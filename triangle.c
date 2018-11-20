e455ba128a346325265cb7389ecae196a0b53538
blue <dylancuprewich@gmail.com>
2018-11-19 19:40:27 -0500

    Added: - added spell_out function;     - displays text out to a window given a position. - added center_text function;     - displays text in the middle of a line in a given window. - added underline_word function;     - addes '-' under a word in a given window. - added center_cursor;     - moves the cursor to the center of a given line in a given window.
    Removed:
    N/a
    
    TODO:
    - refactor triangle.c
        - add prototypes to triangle.h
        - move function documentation to triangle.h, replaced with brief
        documentation
    - everything else....

--- triangle.c
+++ triangle.c
@@ -3,12 +3,15 @@
 #include <stdlib.h> // idk yet, will probably need
 #include <ncurses.h> // for output
 #include <string.h> // for dealing with and manipulating strings 
-#include <unistd.h>
+#include <unistd.h> // usleep
 
 #include "triangle.h" // for prototypes and typedefs
 
+#define SPELL_DELAY 15000
+
 /*
- *
+ * Draws basic border around the screen. Consists of '+' for corners and '-'s and '|' for sides.
+ * @param screen the window to draw the border
  */
 void draw_borders(WINDOW *screen) 
 { 
@@ -32,24 +35,82 @@ void draw_borders(WINDOW *screen)
     wrefresh(screen);
 }
 
+
 /*
- *
+ *  Spells out a word to the specified window char by char.
+ *  @param word the word to be displayed to the window win.
+ *  @param win the window to have word displayed to.
+ *  @param maxy the maximum y value of the window win.
+ *  @param maxx the maximum x value of the window win.
+ */
+void spell_out( char* word, WINDOW* win, int maxy, int maxx)
+{
+    if( maxy+strlen(word) >= getmaxx(win) || maxy == 0)
+    {
+        fprintf( stderr, "%s\n", "String too long to print within borders at position." );
+    }
+    else if( maxy >=  getmaxy( win ) || maxy == 0 )
+    {
+        fprintf( stderr, "%s\n", "String to be printed would either print on border or out of bounds." );
+    }
+    for( unsigned int p = 0; p < strlen(word); p++ )
+    {
+        wmove( win , maxy, maxx + p );
+        wdelch( win );
+        winsch( win, word[p] );
+        wrefresh( win );
+        usleep( SPELL_DELAY );
+    }
+}
+
+/*
+ *  Returns x position to start printing the word out at.
+ *  @param word the word to center.
+ *  @param win the window that the text will be centered in.
+ *  @return the x value to start printing the word at for the word to be centered.
+ */
+int center_text( char* word, WINDOW *win)
+{
+    return ( getmaxx(win) - strlen(word) ) / 2;
+}
+
+/*
+ *  Centers the cursor on a provided y level in a provided window.
+ *  @param win the window to center the cursor in.
+ *  @param y the y level for the cursor to be centered on.
+ */
+void center_cursor( WINDOW *win, int y)
+{
+    wmove( win, y, getmaxx( win )/2 );
+}
+
+void underline_word( char* word, WINDOW* win, int maxy, int maxx )
+{
+    char *underline = malloc( sizeof(word) );
+    strcpy( underline, word );
+    for( unsigned int i = 0; i<strlen(word); i++ )
+    {
+        underline[i]='-';
+    }
+    maxy+=1;
+    spell_out( underline, win, maxy, maxx );
+}
+
+/*
+ * displays title of game and waits for user input
  */
 void title_screen(int maxx, int maxy)
 {
     WINDOW *start = newwin( maxy, maxx,  0 , 0);
     draw_borders( start );
     char* title = "This is a test Title for the unnamed game based in ncurses";
-    for( unsigned int position = 0; position < strlen(title); position++ )
-    {
-        wmove( start, maxy/5, maxx/5 + position );
-        wdelch( start );
-        winsch( start, title[position] );
-        wrefresh(start);
-        usleep( 15000 );
-    }
-    sleep(4);
-    //getch(); // holds until input
+    int x_c = center_text( title, start ); 
+    spell_out( title, start, maxy/5, x_c );
+    underline_word( title, start, maxy/5, x_c );
+    center_cursor( start, maxy/4 );
+    //sleep(4);
+    wgetch( start ); // holds until input
+    usleep( SPELL_DELAY );
     delwin(start);
 }
 
@@ -58,6 +119,7 @@ void title_screen(int maxx, int maxy)
  */
 int main()
 {
+    // initializes curses environment 
     initscr();
     cbreak();
     noecho();
@@ -69,6 +131,7 @@ int main()
     
     // title screen
     title_screen( maxx, maxy );
+
     // proceed
     WINDOW *win;
     WINDOW *another_one;
@@ -99,7 +162,7 @@ int main()
         wmove(current_window, rand()%getmaxy(current_window) , rand()%getmaxx(current_window));
     }
 
-    sleep(3);
+    //sleep(3);
     //title screen
     delwin(win);
     delwin(another_one);
